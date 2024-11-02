d4c31859be1322d4810580b3801bfc2473c757f6
Eugene Vorontsov <sejeenn@gmail.com>
2024-11-02 22:45:15 +0300

    create env-file

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class RunConfig(BaseModel):
@@ -20,9 +20,17 @@ class DatabaseConfig(BaseModel):
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
 
 
 settings = Settings()
+print(settings.db.url)
+print(settings.db.echo)
--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,10 +1,22 @@
+from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 from core.config import settings
+from core.models.db_helper import db_helper
 from api import router as api_router
 
-app = FastAPI()
-app.include_router(
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    print("dispose engine")
+    await db_helper.dispose()
+
+
+main_app = FastAPI(lifespan=lifespan)
+main_app.include_router(
     api_router,
     prefix=settings.api.prefix,
 )
@@ -12,7 +24,7 @@ app.include_router(
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "main:main_app",
         host=settings.run.host,
         port=settings.run.port,
         reload=True,
