3f0c5ba9154375192fc7435444c2e7d010f41fb0
Eugene Vorontsov <sejeenn@gmail.com>
2024-11-04 12:09:24 +0300

    create mixin id_pk

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -1,11 +1,5 @@
 from sqlalchemy import MetaData
-from sqlalchemy.orm import (
-    DeclarativeBase,
-    Mapped,
-    mapped_column,
-    declared_attr
-)
-
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 from core.config import settings
 from utils import camel_case_to_snake_case
 
@@ -20,5 +14,3 @@ class Base(DeclarativeBase):
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f"{camel_case_to_snake_case(cls.__name__)}s"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -3,8 +3,10 @@ from sqlalchemy import UniqueConstraint
 from .base import Base
 from sqlalchemy.orm import Mapped, mapped_column
 
+from .mixins.int_id_pk import IntIdPkMixin
 
-class User(Base):
+
+class User(IntIdPkMixin, Base):
     username: Mapped[str] = mapped_column(unique=True)
     foo: Mapped[int]
     bar: Mapped[int]
